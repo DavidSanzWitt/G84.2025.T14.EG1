913d69142a5b576b167f0e2a386f76eef2de831a
DavidSanzWitt <100522195@alumnos.uc3m.es>
2025-02-05 16:08:41 +0100

    Base de IBAN

--- pattern.py
+++ pattern.py
@@ -1,3 +1,11 @@
-"""
-Created by David Sanz Wittmann in feb 2025
-"""
+
+import re
+
+regex = r"^ES\d{22}$"
+
+# Ejemplo de uso
+cadena = "ES1234567890123456789012"
+if re.match(regex, cadena):
+    print("La cadena cumple con las características.")
+else:
+    print("La cadena no cumple con las características.")
